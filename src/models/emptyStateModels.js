93c2b57439f7cadd60b5bf80912c21465bfb1d19
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-19 08:55:53 -0600

    create empty state models

---
+++ src/models/emptyStateModels.js
@@ -0,0 +1,50 @@
+export const emptyVideosState = [
+  {
+    video: {
+      author: {
+        avatar: [
+          {
+            url: '',
+          },
+        ],
+        channelId: '',
+        title: '',
+      },
+      lengthSeconds: 0,
+      movingThumbnails: [
+        {
+          url: '',
+        },
+      ],
+      publishedTimeText: '',
+      stats: {
+        views: 0,
+      },
+      thumbnails: [
+        {
+          url: '',
+        },
+      ],
+      title: '',
+      videoId: '',
+    },
+  },
+];
+
+export const emptyUserState = {
+  id: '',
+  email: '',
+  user_name: '',
+  picture: '',
+  room_id: '',
+  user_type: '',
+};
+
+export const emptyCategoriesState = [
+  {
+    id: 0,
+    category_name: '',
+  },
+];
+
+export const emptyVideoListState = {};
