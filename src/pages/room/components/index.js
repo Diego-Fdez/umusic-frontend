ed834559bf76c7d287fae666e27b0c8d5ffb4809
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-20 18:19:53 -0600

    remove files

--- src/pages/room/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as PlayList } from './PlayList/PlayList';
-export { default as VideoHeaders } from './VideoHeaders/VideoHeaders';
-export { default as VideoScreen } from './VideoScreen/VideoScreen';
