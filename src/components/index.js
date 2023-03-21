68f0f0c4fe4a5f1d11cd1365ea839583bd3c9aa9
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-20 18:06:39 -0600

    update barrel

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as CategoryScreen } from './CategoryScreen/CategoryScreen';
 export { default as Loader } from './Loader/Loader';
 export { default as Navbar } from './Navbar/Navbar';
+export { default as VideoCard } from './VideoCard/VideoCard';
