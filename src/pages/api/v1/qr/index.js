8a389c7db9f2cd1160304366ba90e0ca2f25f1be
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-24 16:08:57 -0600

    create qr controller

---
+++ src/pages/api/v1/qr/index.js
@@ -0,0 +1,25 @@
+import QRCode from 'qrcode';
+
+//It takes a token and an id, and returns a QR code
+const generateQR = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res
+      .status(405)
+      .send({ status: 'FAILED', data: { error: 'Method not allowed' } });
+  }
+
+  const { token, id } = req.body;
+
+  const url = `${process.env.FRONTEND_URL}/tempauth/${id}/${token}`;
+
+  try {
+    const result = await QRCode.toDataURL(url);
+    res.send({ status: 'OK', data: result });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: 'FAILED', data: { error: error?.message || error } });
+  }
+};
+
+export default generateQR;
