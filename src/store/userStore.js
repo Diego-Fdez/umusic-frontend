c3067eb25f03bfdf5f3ed09ddff56722044b7a5b
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-26 15:13:30 -0600

    remove cookies

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -2,19 +2,28 @@ import { create } from 'zustand';
 import Cookies from 'js-cookie';
 import { emptyUserState } from '@/models/emptyStateModels';
 
+const getCookieToken = Cookies.get('userToken') ? Cookies.get('userToken') : '';
+const setCookieToken = (data) => {
+  Cookies.set('userToken', data);
+};
+// const getCookieUser = Cookies.get('user') ? Cookies.get('user') : '';
+// const setCookieUser = (data) => {
+//   Cookies.set('user', data);
+// };
+
 const userStore = create((set) => ({
-  user: emptyUserState,
-  loadingUser: false,
-  addUser: (data) => {
-    set((state) => ({
-      user: data,
-    }));
+  userToken: getCookieToken,
+  // user: getCookieUser,
+  addUserToken: (data) => {
+    set((state) => {
+      data && setCookieToken(data);
+    });
   },
-  setLoadingUser: (data) => {
-    set((state) => ({
-      loadingUser: data,
-    }));
-  },
+  // addUser: (data) => {
+  //   set((state) => {
+  //     data && setCookieUser(data);
+  //   });
+  // },
 }));
 
 export default userStore;
