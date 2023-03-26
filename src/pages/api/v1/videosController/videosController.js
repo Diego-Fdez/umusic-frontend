d48a3878f621c674618988d165973dbf602ad425
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-26 15:12:00 -0600

    add mongo db connection

--- src/pages/api/v1/videosController/videosController.js
+++ src/pages/api/v1/videosController/videosController.js
@@ -1,26 +1,38 @@
-import { pool } from '@/database/pool';
+import db from '@/database/db';
+import Channels from '@/models/channelsModel';
+import Videos from '@/models/videoModel';
 
 /** It checks if the channel exists in the database, if it does not exist, it inserts the channel into
  * the database */
-export const registerChannel = async (channelId, title, channelPictureURL) => {
+export const registerChannel = async (
+  channelId,
+  title,
+  channelPictureURL,
+  videoId
+) => {
   try {
     //check the channel in the DB
-    const [rows] = await pool.query(
-      `SELECT * FROM channels WHERE id = '${channelId}'`
-    );
+    await db.connect();
 
-    /* Checking if the channel exists in the database. If it does not exist, it will insert the channel
+    const channelExist = await Channels.findOne({ channelId });
+
+    /* Checking if the channel exists in the database, if it does not exist, it inserts the channel
     into the database. */
-    if (rows.length <= 0)
-      await pool.query(
-        'INSERT INTO channels (id, channel_title, channel_pic_url) VALUES (?,?,?)',
-        [channelId, title, channelPictureURL]
-      );
+    if (!channelExist) {
+      const newChannel = new Channels({
+        channel_id: channelId,
+        channel_title: title,
+        channel_pic_url: channelPictureURL,
+        video_id: videoId,
+      });
+
+      await newChannel.save();
+      await db.disconnect();
+    }
+
+    await db.disconnect();
   } catch (error) {
-    if (error.code !== 'ER_DUP_ENTRY')
-      res
-        .status(error?.status || 500)
-        .send({ status: 'FAILED', data: { error: error?.message || error } });
+    throw error;
   }
 };
 
@@ -28,25 +40,32 @@ export const registerChannel = async (channelId, title, channelPictureURL) => {
  * the database */
 export const registerVideo = async (
   videoId,
-  channelId,
   videoTitle,
   videoPictureURL,
   videoLength
 ) => {
   try {
     //check the video in the DB
-    const [rows] = await pool.query(
-      `SELECT * FROM videos WHERE id = '${videoId}'`
-    );
+    await db.connect();
 
-    /* Checking if the video exists in the database. If it does not exist, it will insert the video
-    into the database. */
-    if (rows.length <= 0)
-      await pool.query(
-        'INSERT INTO videos (id, channel_id, video_title, video_pic_url, video_length) VALUES (?,?,?,?,?)',
-        [videoId, channelId, videoTitle, videoPictureURL, videoLength]
-      );
+    const videoExist = await Videos.findOne({ videoId });
+
+    /* Checking if the video exists in the database, if it does not exist, it inserts the video into
+     * the database. */
+    if (!videoExist) {
+      const newVideo = new Videos({
+        video_id: videoId,
+        video_title: videoTitle,
+        video_pic_url: videoPictureURL,
+        video_length: videoLength,
+      });
+
+      await newVideo.save();
+      await db.disconnect();
+    }
+
+    await db.disconnect();
   } catch (error) {
-    if (error.code !== 'ER_DUP_ENTRY') throw error;
+    throw error;
   }
 };
