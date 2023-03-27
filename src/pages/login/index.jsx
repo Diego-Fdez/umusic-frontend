1228eb1e897cfda9066822c1a07bff52066ff4db
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-26 19:54:03 -0600

    remove loader

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -22,34 +22,30 @@ const Login = () => {
         <meta charset='utf-8' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <div className={styles.loginContainer}>
-          <Link href={'/'}>
-            <h2 className={styles.loginLogo}>UMUSIC</h2>
-          </Link>
-          <div className={styles.loginWrapper}>
-            <h1 className={styles.loginTitle}>
-              Connect to <span className={styles.loginTitleSpan}>U</span>MUSIC
-            </h1>
-            <p className={styles.loginSubtitle}>
-              Sign in, listen to your music, create playlists and share with
-              your friends.
-            </p>
-          </div>
-          <div className={styles.loginButtonsContainer}>
-            <button onClick={() => loginWithRedirect()}>
-              <img
-                src='/auth0-icon.svg'
-                alt='auth0-logo'
-                className={styles.logoImg}
-              />
-              Login
-            </button>
-          </div>
+      <div className={styles.loginContainer}>
+        <Link href={'/'}>
+          <h2 className={styles.loginLogo}>UMUSIC</h2>
+        </Link>
+        <div className={styles.loginWrapper}>
+          <h1 className={styles.loginTitle}>
+            Connect to <span className={styles.loginTitleSpan}>U</span>MUSIC
+          </h1>
+          <p className={styles.loginSubtitle}>
+            Sign in, listen to your music, create playlists and share with your
+            friends.
+          </p>
         </div>
-      )}
+        <div className={styles.loginButtonsContainer}>
+          <button onClick={() => loginWithRedirect()}>
+            <img
+              src='/auth0-icon.svg'
+              alt='auth0-logo'
+              className={styles.logoImg}
+            />
+            Login
+          </button>
+        </div>
+      </div>
     </Suspense>
   );
 };
