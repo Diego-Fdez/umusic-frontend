c0d0c194bcb72efbd193275b491ed3ed65714875
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-31 18:36:16 -0600

    add middleware

---
+++ src/middlewares/checkJwt.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import jwksClient from 'jwks-rsa';
+
+/* Getting the public key from the jwks.json file. */
+const client = jwksClient({
+  jwksUri: process.env.JWKSURI,
+  requestHeaders: {},
+  timeout: 30000,
+});
+
+export async function verifyToken(token) {
+  const kid = process.env.KID; //decoded.header.kid;
+  const key = await client.getSigningKey(kid);
+  const signingKey = key.getPublicKey();
+
+  try {
+    /* Verifying the token. */
+    jwt.verify(token, signingKey, { algorithms: ['RS256'] });
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
