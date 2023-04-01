52ecb8579b8a207f9511bebdd2b3f70cfa8876cf
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-01 16:28:22 -0600

    add shorter link function

--- src/pages/api/v1/qr/index.js
+++ src/pages/api/v1/qr/index.js
@@ -1,5 +1,40 @@
 import QRCode from 'qrcode';
 
+//It takes a long URL and returns a shortened URL
+const getShortenedURL = async (longURL) => {
+  /* Creating a new URL object. */
+  const url = new URL('https://t.ly/api/v1/link/shorten');
+
+  /* Setting the api_token to the value of the SHORTER_API_KEY environment variable. */
+  const params = {
+    api_token: process.env.SHORTER_API_KEY,
+  };
+
+  /* Adding the api_token to the URL. */
+  Object.keys(params).forEach((key) =>
+    url.searchParams.append(key, params[key])
+  );
+
+  /* Setting the headers of the request. */
+  const headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  };
+
+  /* Converting the data to JSON format. */
+  const setData = JSON.stringify({
+    long_url: longURL,
+    domain: 'https://t.ly/',
+    include_qr_code: false,
+  });
+
+  return fetch(url, {
+    method: 'POST',
+    headers,
+    body: setData,
+  }).then((response) => response.json());
+};
+
 //It takes a token and an id, and returns a QR code
 const generateQR = async (req, res) => {
   if (req.method !== 'POST') {
@@ -12,9 +47,15 @@ const generateQR = async (req, res) => {
 
   const url = `${process.env.FRONTEND_URL}/tempauth/${id}/${room}/${token}`;
 
+  const shortenedURL = await getShortenedURL(url);
+
   try {
-    const result = await QRCode.toDataURL(url);
-    res.send({ status: 'OK', data: result });
+    const qrImage = await QRCode.toDataURL(shortenedURL?.short_url);
+
+    res.send({
+      status: 'OK',
+      data: { qrImage, linkURL: shortenedURL?.short_url },
+    });
   } catch (error) {
     res
       .status(error?.status || 500)
