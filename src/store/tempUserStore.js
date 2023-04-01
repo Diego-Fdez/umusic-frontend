f2df0f7b8b903f475b90804568c17b3334f06041
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-31 18:37:19 -0600

    create tempUserStore

---
+++ src/store/tempUserStore.js
@@ -0,0 +1,36 @@
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+
+const tempUserStore = create(
+  persist(
+    (set, get) => ({
+      tempUserToken: '',
+      tempUserInfo: {},
+      tempUserIsAuth: false,
+      addTempUserToken: (token) => {
+        set({ tempUserToken: token });
+      },
+      addTempUserInfo: (user) => {
+        set({ tempUserInfo: user });
+      },
+      setTempUserIsAuth: (data) => {
+        set({ tempUserIsAuth: data });
+      },
+      getTempUserToken: () => {
+        return get().tempUserToken;
+      },
+      getTempUserInfo: () => {
+        return get().tempUserInfo;
+      },
+      getTempUserIsAuth: () => {
+        return get().tempUserIsAuth;
+      },
+    }),
+    {
+      name: 'umusic-storage',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
+
+export default tempUserStore;
