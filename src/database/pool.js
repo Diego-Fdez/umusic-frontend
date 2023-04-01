9501c5697172c91a06262ac73b118c9c582e0c9f
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-31 18:35:47 -0600

    remove files

--- src/database/pool.js
+++
@@ -1,9 +0,0 @@
-import { createPool } from 'mysql2/promise';
-
-export const pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
