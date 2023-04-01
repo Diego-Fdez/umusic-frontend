82c4ce61a904107b359b53ebea7b716933ffaa8e
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-01 11:12:56 -0600

    import google analytics

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export { default as CategoryScreen } from './CategoryScreen/CategoryScreen';
+export { default as GoogleAnalytics } from './GoogleAnalytics/GoogleAnalytics';
+export { default as HeadScreen } from './Head/HeadScreen';
 export { default as Loader } from './Loader/Loader';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as VideoCard } from './VideoCard/VideoCard';
