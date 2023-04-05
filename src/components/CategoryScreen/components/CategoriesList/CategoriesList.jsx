a3e5a0e8d0e1f645b19d30f3104bceff962da74a
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-05 16:36:11 -0600

    fetch categories with swr

--- src/components/CategoryScreen/components/CategoriesList/CategoriesList.jsx
+++ src/components/CategoryScreen/components/CategoriesList/CategoriesList.jsx
@@ -1,45 +1,25 @@
-import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 import styles from './styles/CategoriesList.module.css';
-import UseFetchFromDB from '@/hooks/useFetchFromDB';
 import { Loader } from '@/components';
-import persistedVideoStore from '@/store/persistedVideoStore';
 import videoStore from '@/store/videoStore';
 
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 const CategoriesList = () => {
-  const { fetchFromDB, loading } = UseFetchFromDB();
-  const addCategories = persistedVideoStore((state) => state.addCategories);
-  const categoriesState = persistedVideoStore((state) => state.categoriesState);
   const keyword = videoStore((state) => state.addKeyword);
-  const [categories, setCategories] = useState([]);
 
-  //It fetches the categories from the database and adds them to the state
-  async function handleGetCategories() {
-    try {
-      const result = await fetchFromDB(`/api/v1/categories`, 'GET');
-      /* Adding the categories to the state. */
-      addCategories(result?.data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  /* Setting the categories to the categoriesState. */
-  useEffect(() => {
-    setCategories(categoriesState);
-  }, [categoriesState]);
-
-  /* Checking if the categories array is empty and if it is, it calls the handleGetCategories function. */
-  useEffect(() => {
-    categories?.length === 0 && handleGetCategories();
-  }, [categories]);
+  const { data, error, isLoading } = useSWR('/api/v1/categories', fetcher, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
 
   return (
     <>
-      {loading ? (
+      {isLoading ? (
         <Loader />
       ) : (
         <aside className={styles.categoriesContainer}>
-          {categories?.map((category) => (
+          {data?.data?.map((category) => (
             <button
               key={category?.id}
               className={styles.categoryButton}
