9faed36e7d889f86b095cf5220ed36f21967981f
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-05 12:34:24 -0600

    add function to delete a playlist

--- src/pages/room/components/ListScreen/ListScreen.jsx
+++ src/pages/room/components/ListScreen/ListScreen.jsx
@@ -6,6 +6,7 @@ import UseFetchFromDB from '@/hooks/useFetchFromDB';
 import videoStore from '@/store/videoStore';
 import userStore from '@/store/userStore';
 import persistedVideoStore from '@/store/persistedVideoStore';
+import { Loader } from '@/components';
 
 const ListScreen = () => {
   const { fetchFromDB, loading, error } = UseFetchFromDB();
@@ -17,7 +18,7 @@ const ListScreen = () => {
   );
 
   //It fetches the playlists from the database.
-  const fetchPlaylists = async () => {
+  const handlerGetAllPlaylists = async () => {
     /* Checking if the user is logged in. If not, it will display a toast message. */
     if (!user) return toast.info('Please login to view your playlists');
 
@@ -33,26 +34,51 @@ const ListScreen = () => {
   };
 
   useEffect(() => {
-    if (rooms.length === 0) fetchPlaylists();
+    if (rooms.length === 0) handlerGetAllPlaylists();
   }, [rooms]);
+
+  //function to delete a playlist.
+  const deleteOnePlaylist = async (roomId) => {
+    const result = await fetchFromDB(`/api/v1/room/${roomId}`, 'DELETE');
+
+    //if the result is an error, display it.
+    if (result?.data?.error) return toast.error(result?.data?.error);
+    if (error) return toast.error(error);
+
+    //filter the rooms array to remove the deleted playlist.
+    const filteredRooms = rooms.filter((room) => room._id !== roomId);
+    setRooms(filteredRooms);
+    setCurrentPlaylist(filteredRooms[0]);
+    toast.success('Playlist deleted successfully');
+  };
 
   return (
     <ul className={styles.listContainer}>
-      {rooms.map((room) => (
-        <li key={room._i}>
-          <Link href={`/room/${room._id}`}>{room?.room_name}</Link>
-          <div>
-            <button className={styles.btn} title='Delete Playlist'>
-              Delete
-              <img
-                src='/delete-playlist.svg'
-                alt='delete-playlist-icon'
-                className={styles.deletePlaylistIcon}
-              />
-            </button>
-          </div>
-        </li>
-      ))}
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {rooms.map((room) => (
+            <li key={room._i}>
+              <Link href={`/room/${room._id}`}>{room?.room_name}</Link>
+              <div>
+                <button
+                  className={styles.btn}
+                  title='Delete Playlist'
+                  onClick={() => deleteOnePlaylist(room._id)}
+                >
+                  Delete
+                  <img
+                    src='/delete-playlist.svg'
+                    alt='delete-playlist-icon'
+                    className={styles.deletePlaylistIcon}
+                  />
+                </button>
+              </div>
+            </li>
+          ))}
+        </>
+      )}
     </ul>
   );
 };
