a0c916e698c06acc07c7b3339002628b66cc5fc7
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-05 12:33:04 -0600

    add deleteAllVideos function

--- src/pages/api/v1/room/[id]/index.js
+++ src/pages/api/v1/room/[id]/index.js
@@ -80,36 +80,54 @@ const getRoomById = async (req, res) => {
   }
 };
 
-//It deletes a video from the room list
-const deleteVideoFromRoomList = async (req, res) => {
+// This function deletes all videos from a playlist in a MongoDB database.
+const deleteAllVideosFromRoom = async (req, res) => {
   const { id } = req.query;
 
   try {
     await db.connect();
 
-    /* Getting the room by the id. */
-    const videoExist = await Room.findById(id);
+    //This code is deleting all videos from a playlist in a MongoDB database.
+    const result = await Room.updateOne(
+      { _id: id },
+      { $unset: { video_id: '' } }
+    );
 
-    /* It checks if the video exists. If it doesn't, it sends a message to the frontend. */
-    if (!video) {
-      res.status(404).send({
+    /*it sends a 404 status code with a message indicating that
+    there are no videos in the playlist. */
+    if (!result) {
+      return res.status(404).send({
         status: 'FAILED',
-        data: { error: 'Video not found' },
+        data: 'There are no videos in this playlist.',
       });
-      return;
     }
 
-    //if it passes the validations, the video is removed
-    videoExist.videos_id.pull(req.body.id);
-    await videoExist.save();
+    await db.disconnect();
+    res.status(200).send({ status: 'OK', data: 'Deleted all videos' });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: 'FAILED', data: { error: error?.message || error } });
+  }
+};
+
+const deleteRoom = async (req, res) => {
+  const { id } = req.query;
+
+  try {
+    await db.connect();
+
+    const playlistExist = await Room.deleteOne({ _id: id });
+
+    if (!playlistExist) {
+      return res.status(404).send({
+        status: 'FAILED',
+        data: 'Playlist not found',
+      });
+    }
 
     await db.disconnect();
-
-    //sending the response to the frontend
-    res.status(200).send({
-      status: 'OK',
-      data: 'Video removed from the list.',
-    });
+    res.status(200).send({ status: 'OK', data: 'Playlist deleted' });
   } catch (error) {
     res
       .status(error?.status || 500)
@@ -118,10 +136,12 @@ const deleteVideoFromRoomList = async (req, res) => {
 };
 
 const handler = (req, res) => {
-  if (req.method === 'DELETE') {
-    return deleteVideoFromRoomList(req, res);
-  } else if (req.method === 'GET') {
+  if (req.method === 'GET') {
     return getRoomById(req, res);
+  } else if (req.method === 'PUT') {
+    return deleteAllVideosFromRoom(req, res);
+  } else if (req.method === 'DELETE') {
+    return deleteRoom(req, res);
   } else {
     return res
       .status(405)
