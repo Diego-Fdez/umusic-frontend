7175dcc510658a0e1dbbc93eac484cc125b67760
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-05 16:16:02 -0600

    remove fetch from youtube

--- src/components/CategoryScreen/CategoryScreen.jsx
+++ src/components/CategoryScreen/CategoryScreen.jsx
@@ -1,11 +1,6 @@
-import { useFetch } from '@/hooks/useFetchFromYoutube';
-import videoStore from '@/store/videoStore';
 import { CategoriesList } from './components';
 
 const CategoryScreen = () => {
-  const keyword = videoStore((state) => state.keyword);
-  useFetch(`v1/search/?q=${keyword}&hl=en&gl=US`);
-
   return <CategoriesList />;
 };
 
