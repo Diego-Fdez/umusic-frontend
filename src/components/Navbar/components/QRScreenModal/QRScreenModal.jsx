ab6edf746ca496c6a061375b313549d8c9c719d5
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-05 21:34:00 -0600

    remove shortner

--- src/components/Navbar/components/QRScreenModal/QRScreenModal.jsx
+++ src/components/Navbar/components/QRScreenModal/QRScreenModal.jsx
@@ -60,7 +60,7 @@ const QRScreenModal = ({ modalOpen, setModalOpen }) => {
       <div className={styles.qrBox}>
         <img src={qrImage} alt='qr-image' />
         <div className={styles.qrTextContainer}>
-          <p>{qrDataURL}</p>
+          <p>{qrDataURL.substring(0, 26)}</p>
           <button onClick={() => navigator.clipboard.writeText(qrDataURL)}>
             <p>Copy Link</p>
             <img
