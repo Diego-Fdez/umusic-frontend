16c12698d246c74566f2da44b1d47d9b19c68098
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-12 18:27:57 -0600

    separated component

---
+++ src/pages/watch/components/VideoPlayerInfo/VideoPlayerInfo.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import styles from "./styles/VideoPlayerInfo.module.css";
+
+const VideoPlayerInfo = ({ selectedVideo }) => {
+  return (
+    <div className={styles.videoInfo}>
+      <h4 className={styles.videoInfoTitle}>{selectedVideo?.video?.title}</h4>
+      <div className={styles.infoAuthorContainer}>
+        <Image
+          src={selectedVideo?.video?.author?.avatar[0]?.url}
+          alt={selectedVideo?.video?.author?.title}
+          className={styles.infoAuthorAvatar}
+          width={32}
+          height={32}
+        />
+        <h6 className={styles.videoInfoSubtitle}>
+          {selectedVideo?.video?.author?.title}
+        </h6>
+      </div>
+      <div className={styles.infoStatsContainer}>
+        <span>
+          {selectedVideo?.video?.stats?.views
+            ? `${Number(selectedVideo?.video?.stats?.views).toLocaleString(
+                "es-US"
+              )} views `
+            : "0 views"}
+        </span>
+        <span>
+          {selectedVideo?.video?.publishedTimeText
+            ? ` - ${selectedVideo?.video?.publishedTimeText}`
+            : ""}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default VideoPlayerInfo;
