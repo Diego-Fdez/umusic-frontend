d6d3a364319f3359e972c4e4bee914ebdc70c68d
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-13 16:51:29 -0600

    change function name

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image";
 import { toast } from "react-toastify";
 import styles from "./styles/videoCard.module.css";
 import { formattedTime } from "@/utils/formattedTime";
@@ -8,6 +7,7 @@ import { setDataVideo } from "@/models/dataFetchModels";
 import persistedVideoStore from "@/store/persistedVideoStore";
 import tempUserStore from "@/store/tempUserStore";
 import userStore from "@/store/userStore";
+import VideoInfoScreen from "./components/VideoInfoScreen/VideoInfoScreen";
 
 const VideoCard = ({ video, socket }) => {
   const { fetchFromDB, error } = UseFetchFromDB();
@@ -17,7 +17,7 @@ const VideoCard = ({ video, socket }) => {
 
   /** It creates an object with the data that will be sent to the database, and then calls the
    * `fetchFromDB` function from the `UseFetchFromDB` hook */
-  const handleClick = async () => {
+  const handleAddVideoToList = async () => {
     /* Creating an object with the data that will be sent to the database. */
     const setData = setDataVideo(
       user ? user : tempUserInfo,
@@ -56,66 +56,30 @@ const VideoCard = ({ video, socket }) => {
   };
 
   return (
-    <>
-      <div className={styles.videoCardContainer}>
-        <Link href={`/watch/${video?.videoId}`}>
-          <img
-            src={video?.thumbnails[0]?.url}
-            alt={video?.videoId}
-            loading='lazy'
-            className={styles.videoCardImage}
-            onMouseOver={(e) =>
-              (e.target.src =
-                video?.movingThumbnails !== null
-                  ? video?.movingThumbnails[0]?.url
-                  : video?.thumbnails[0]?.url)
-            }
-            onMouseLeave={(e) => (e.target.src = video?.thumbnails[0]?.url)}
-          />
-        </Link>
-        <div className={styles.durationContainer}>
-          <p>{formattedTime(video?.lengthSeconds)}</p>
-        </div>
-        <div className={styles.videoCardTitle}>
-          <Link href={`/watch/${video?.videoId}`}>
-            <h4 className={styles.title}>{video?.title.slice(0, 60)}</h4>
-          </Link>
-          <div className={styles.authorContainer}>
-            <Image
-              src={video?.author?.avatar[0]?.url}
-              alt={video?.author?.title}
-              className={styles.authorImage}
-              width={24}
-              height={24}
-            />
-            <Link href={`/channel/${video?.author?.channelId}`}>
-              <h6 className={styles.subtitle}>{video?.author?.title}</h6>
-            </Link>
-          </div>
-          <div className={styles.statsContainer}>
-            <span>
-              {video?.stats?.views
-                ? `${Number(video?.stats?.views).toLocaleString(
-                    "es-US"
-                  )} views `
-                : "0 views"}
-            </span>
-            <span>
-              {video?.publishedTimeText ? ` - ${video?.publishedTimeText}` : ""}
-            </span>
-            <button className={styles.addButton} onClick={() => handleClick()}>
-              <Image
-                src='/add-icon.svg'
-                alt='add-icon'
-                width={100}
-                height={100}
-                className={styles.addIcon}
-              />
-            </button>
-          </div>
-        </div>
+    <div className={styles.videoCardContainer}>
+      <Link href={`/watch/${video?.videoId}`}>
+        <img
+          src={video?.thumbnails[0]?.url}
+          alt={video?.videoId}
+          loading='lazy'
+          className={styles.videoCardImage}
+          onMouseOver={(e) =>
+            (e.target.src =
+              video?.movingThumbnails !== null
+                ? video?.movingThumbnails[0]?.url
+                : video?.thumbnails[0]?.url)
+          }
+          onMouseLeave={(e) => (e.target.src = video?.thumbnails[0]?.url)}
+        />
+      </Link>
+      <div className={styles.durationContainer}>
+        <p>{formattedTime(video?.lengthSeconds)}</p>
       </div>
-    </>
+      <VideoInfoScreen
+        video={video}
+        handleAddVideoToList={handleAddVideoToList}
+      />
+    </div>
   );
 };
 
