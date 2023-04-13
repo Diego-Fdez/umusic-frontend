9a7b796c706dfc8ffbc5fad90717c1ce500c1976
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-12 18:27:19 -0600

    add button to next video

--- src/pages/watch/[id].js
+++ src/pages/watch/[id].js
@@ -1,17 +1,21 @@
-import { Suspense, useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import Image from 'next/image';
-import ReactPlayer from 'react-player';
-import styles from './styles/PlayerScreen.module.css';
-import videoStore from '@/store/videoStore';
-import Sidebar from './components/Sidebar/Sidebar';
-import { Navbar, Loader, GoogleAnalytics, HeadScreen } from '@/components';
+import { Suspense, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import ReactPlayer from "react-player";
+import styles from "./styles/PlayerScreen.module.css";
+import videoStore from "@/store/videoStore";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Navbar, Loader, GoogleAnalytics, HeadScreen } from "@/components";
+import VideoPlayerInfo from "./components/VideoPlayerInfo/VideoPlayerInfo";
 
 const PlayerScreen = () => {
-  const { query } = useRouter();
-  const { id } = query;
+  const router = useRouter();
+  const { id } = router.query;
   const videos = videoStore((state) => state.videos);
   const [selectedVideo, setSelectedVideo] = useState({});
+
+  function handlerNextVideo() {
+    router.push(`/watch/${videos[0]?.video?.videoId}`);
+  }
 
   /**
    * Find the video with the id that matches the id passed in as an argument and set that video as the
@@ -43,45 +47,18 @@ const PlayerScreen = () => {
         <div className={styles.playListVideoWrapper}>
           <article className={styles.playerContainer}>
             <ReactPlayer
-              url={`https://www.youtube.com/watch?v=${id}`}
+              url={`https://www.youtube.com/watch?v=${selectedVideo?.video?.videoId}`}
               width='100%'
               height='100%'
               controls={true}
-              loop
               playing={true}
+              onEnded={handlerNextVideo}
             />
+            <button onClick={() => handlerNextVideo()}>
+              <img src='/next-icon.svg' alt='next-icon' />
+            </button>
           </article>
-          <div className={styles.videoInfo}>
-            <h4 className={styles.videoInfoTitle}>
-              {selectedVideo?.video?.title}
-            </h4>
-            <div className={styles.infoAuthorContainer}>
-              <Image
-                src={selectedVideo?.video?.author?.avatar[0]?.url}
-                alt={selectedVideo?.video?.author?.title}
-                className={styles.infoAuthorAvatar}
-                width={32}
-                height={32}
-              />
-              <h6 className={styles.videoInfoSubtitle}>
-                {selectedVideo?.video?.author?.title}
-              </h6>
-            </div>
-            <div className={styles.infoStatsContainer}>
-              <span>
-                {selectedVideo?.video?.stats?.views
-                  ? `${Number(
-                      selectedVideo?.video?.stats?.views
-                    ).toLocaleString('es-US')} views `
-                  : '0 views'}
-              </span>
-              <span>
-                {selectedVideo?.video?.publishedTimeText
-                  ? ` - ${selectedVideo?.video?.publishedTimeText}`
-                  : ''}
-              </span>
-            </div>
-          </div>
+          <VideoPlayerInfo selectedVideo={selectedVideo} />
         </div>
         <aside>
           <Sidebar videoId={id} />
