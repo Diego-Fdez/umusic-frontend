b4a6cc1aa5f8868e7955f2fb51390b0c09b85954
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-13 16:52:27 -0600

    export baseURL

---
+++ src/utils/youtubeConfig.js
@@ -0,0 +1,9 @@
+export const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
+    "X-RapidAPI-Host": process.env.NEXT_PUBLIC_RAPID_API_HOST,
+  },
+};
+
+export const baseURL = "https://youtube138.p.rapidapi.com";
