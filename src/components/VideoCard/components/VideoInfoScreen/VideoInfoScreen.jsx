3998a4e0d3fce89029200ce7a2f372460ad5b1b2
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-14 13:05:44 -0600

    validate pathname

--- src/components/VideoCard/components/VideoInfoScreen/VideoInfoScreen.jsx
+++ src/components/VideoCard/components/VideoInfoScreen/VideoInfoScreen.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "./styles/VideoInfoScreen.module.css";
 import userStore from "@/store/userStore";
 import tempUserStore from "@/store/tempUserStore";
 
 const VideoInfoScreen = ({ video, handleAddVideoToList }) => {
+  const router = useRouter();
   const user = userStore((state) => state.userInfo);
   const tempUserInfo = tempUserStore((state) => state.tempUserInfo);
 
@@ -13,18 +15,22 @@ const VideoInfoScreen = ({ video, handleAddVideoToList }) => {
       <Link href={`/watch/${video?.videoId}`}>
         <h4 className={styles.title}>{video?.title.slice(0, 60)}</h4>
       </Link>
-      <div className={styles.authorContainer}>
-        <Image
-          src={video?.author?.avatar[0]?.url}
-          alt={video?.author?.title}
-          className={styles.authorImage}
-          width={24}
-          height={24}
-        />
-        <Link href={`/channel/${video?.author?.channelId}`}>
-          <h6 className={styles.subtitle}>{video?.author?.title}</h6>
-        </Link>
-      </div>
+      {router?.pathname === "/channel/[id]" ? (
+        <></>
+      ) : (
+        <div className={styles.authorContainer}>
+          <Image
+            src={video?.author?.avatar[0]?.url}
+            alt={video?.author?.title}
+            className={styles.authorImage}
+            width={24}
+            height={24}
+          />
+          <Link href={`/channel/${video?.author?.channelId}`}>
+            <h6 className={styles.subtitle}>{video?.author?.title}</h6>
+          </Link>
+        </div>
+      )}
       <div className={styles.statsContainer}>
         <span>
           {video?.stats?.views
