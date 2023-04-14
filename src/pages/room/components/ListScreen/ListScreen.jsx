5b7bacc24730da640f789cd62233de6c4b31d74f
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-14 13:06:53 -0600

    show message when room is empty

--- src/pages/room/components/ListScreen/ListScreen.jsx
+++ src/pages/room/components/ListScreen/ListScreen.jsx
@@ -1,12 +1,12 @@
-import { useEffect } from 'react';
-import Link from 'next/link';
-import { toast } from 'react-toastify';
-import styles from './styles/ListScreen.module.css';
-import UseFetchFromDB from '@/hooks/useFetchFromDB';
-import videoStore from '@/store/videoStore';
-import userStore from '@/store/userStore';
-import persistedVideoStore from '@/store/persistedVideoStore';
-import { Loader } from '@/components';
+import { useEffect } from "react";
+import Link from "next/link";
+import { toast } from "react-toastify";
+import styles from "./styles/ListScreen.module.css";
+import UseFetchFromDB from "@/hooks/useFetchFromDB";
+import videoStore from "@/store/videoStore";
+import userStore from "@/store/userStore";
+import persistedVideoStore from "@/store/persistedVideoStore";
+import { Loader } from "@/components";
 
 const ListScreen = () => {
   const { fetchFromDB, loading, error } = UseFetchFromDB();
@@ -20,9 +20,9 @@ const ListScreen = () => {
   //It fetches the playlists from the database.
   const handlerGetAllPlaylists = async () => {
     /* Checking if the user is logged in. If not, it will display a toast message. */
-    if (!user) return toast.info('Please login to view your playlists');
+    if (!user) return toast.info("Please login to view your playlists");
 
-    const result = await fetchFromDB('/api/v1/my-rooms', 'POST', {
+    const result = await fetchFromDB("/api/v1/my-rooms", "POST", {
       userId: user?.sub,
     });
 
@@ -39,7 +39,7 @@ const ListScreen = () => {
 
   //function to delete a playlist.
   const deleteOnePlaylist = async (roomId) => {
-    const result = await fetchFromDB(`/api/v1/room/${roomId}`, 'DELETE');
+    const result = await fetchFromDB(`/api/v1/room/${roomId}`, "DELETE");
 
     //if the result is an error, display it.
     if (result?.data?.error) return toast.error(result?.data?.error);
@@ -49,34 +49,40 @@ const ListScreen = () => {
     const filteredRooms = rooms.filter((room) => room._id !== roomId);
     setRooms(filteredRooms);
     setCurrentPlaylist(filteredRooms[0]);
-    toast.success('Playlist deleted successfully');
+    toast.success("Playlist deleted successfully");
   };
 
   return (
     <ul className={styles.listContainer}>
-      {loading ? (
-        <Loader />
+      {rooms.length === 0 ? (
+        <h2>There are no playlists yet, please create one.</h2>
       ) : (
         <>
-          {rooms.map((room) => (
-            <li key={room._i}>
-              <Link href={`/room/${room._id}`}>{room?.room_name}</Link>
-              <div>
-                <button
-                  className={styles.btn}
-                  title='Delete Playlist'
-                  onClick={() => deleteOnePlaylist(room._id)}
-                >
-                  Delete
-                  <img
-                    src='/delete-playlist.svg'
-                    alt='delete-playlist-icon'
-                    className={styles.deletePlaylistIcon}
-                  />
-                </button>
-              </div>
-            </li>
-          ))}
+          {loading ? (
+            <Loader />
+          ) : (
+            <>
+              {rooms.map((room) => (
+                <li key={room._i}>
+                  <Link href={`/room/${room._id}`}>{room?.room_name}</Link>
+                  <div>
+                    <button
+                      className={styles.btn}
+                      title='Delete Playlist'
+                      onClick={() => deleteOnePlaylist(room._id)}
+                    >
+                      Delete
+                      <img
+                        src='/delete-playlist.svg'
+                        alt='delete-playlist-icon'
+                        className={styles.deletePlaylistIcon}
+                      />
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </>
+          )}
         </>
       )}
     </ul>
