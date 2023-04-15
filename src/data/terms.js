548fe1b55c79959bbd232383945be5ea68410a2b
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-15 17:39:20 -0600

    add terms page

---
+++ src/data/terms.js
@@ -0,0 +1,34 @@
+export const terms = `Condiciones del servicio para Umusic:
+
+Introducción
+Estas Condiciones del Servicio rigen el uso de Umusic, la cual permite a los usuarios crear y compartir listas de reproducción a través del API de YouTube. Al utilizar la aplicación, aceptas estas Condiciones en su totalidad. Si no estás de acuerdo con alguna de estas Condiciones, no debes utilizar la aplicación.
+
+Uso de la aplicación
+a. La aplicación te permite crear y compartir listas de reproducción utilizando el API de YouTube. Al utilizar la aplicación, debes cumplir con todas las políticas y términos de servicio de YouTube.
+
+b. La aplicación se proporciona "tal cual" y no se garantiza su disponibilidad o funcionalidad en todo momento. No seremos responsables por ningún daño que surja del uso de la aplicación.
+
+c. Al utilizar la aplicación, debes cumplir con todas las leyes y regulaciones aplicables.
+
+d. La aplicación se reserva el derecho de eliminar cualquier lista de reproducción o contenido que infrinja los derechos de autor o cualquier otra ley o regulación aplicable.
+
+Propiedad intelectual
+a. Todos los derechos de propiedad intelectual relacionados con la aplicación y su contenido son propiedad de Umusic.
+
+b. No se permite la reproducción, distribución o modificación del contenido de la aplicación sin el consentimiento previo por escrito de Umusic.
+
+Privacidad
+a. La aplicación recopila información personal, incluyendo tu nombre y dirección de correo electrónico, para permitirte crear una cuenta y utilizar la aplicación.
+
+b. La información personal que proporcionas será utilizada de acuerdo con nuestra Política de Privacidad, la cual se encuentra disponible en [enlace a la política de privacidad].
+
+Modificaciones de las Condiciones
+Nos reservamos el derecho de modificar estas Condiciones en cualquier momento. Cualquier modificación será efectiva inmediatamente después de su publicación en la aplicación. Si no estás de acuerdo con las modificaciones, debes dejar de utilizar la aplicación.
+
+Terminación
+Nos reservamos el derecho de terminar tu acceso a la aplicación en cualquier momento y por cualquier motivo. En caso de terminación, todas las disposiciones de estas Condiciones seguirán siendo aplicables.
+
+Ley aplicable
+Estas Condiciones se rigen por las leyes del Costa Rica, sin tener en cuenta sus conflictos de leyes. Cualquier disputa relacionada con estas Condiciones se resolverá en los tribunales de Puntarenas.
+
+Si tienes alguna pregunta o comentario sobre estas Condiciones, no dudes en ponerte en contacto con nosotros en info.diegodev@gmail.com.`;
