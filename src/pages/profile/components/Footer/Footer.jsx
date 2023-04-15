c996b93691d5ab66fb429bcfffcb324c863a9e45
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-15 17:41:15 -0600

    add footer component

---
+++ src/pages/profile/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles/footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2023 Umusic by Diego Fedez</p>
+    </footer>
+  );
+};
+
+export default Footer;
