71dc9264782e233791f0dbbe8c0525b212a76b4e
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-16 17:40:29 -0600

    remove categories from videoStore

--- src/store/persistedVideoStore.js
+++ src/store/persistedVideoStore.js
@@ -1,26 +1,19 @@
-import { create } from 'zustand';
-import { persist, createJSONStorage } from 'zustand/middleware';
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 const persistedVideoStore = create(
   persist(
     (set, get) => ({
-      categoriesState: [],
       currentPlaylist: {},
-      addCategories: (categories) => {
-        set({ categoriesState: categories });
-      },
       setCurrentPlaylist: (data) => {
         set({ currentPlaylist: data });
-      },
-      getCategories: () => {
-        return get().categoriesState;
       },
       getCurrentPlaylist: () => {
         return get().currentPlaylist;
       },
     }),
     {
-      name: 'umusic-storage',
+      name: "umusic-storage",
       storage: createJSONStorage(() => sessionStorage),
     }
   )
