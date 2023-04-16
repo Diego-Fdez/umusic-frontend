e98bc78cfeec11cb95b08847869d18c8be883e5e
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-16 17:37:01 -0600

    create new model

--- src/models/dataFetchModels.js
+++ src/models/dataFetchModels.js
@@ -13,3 +13,23 @@ export function setDataVideo(user, video, playlistId) {
     videoLength: video?.lengthSeconds,
   };
 }
+
+//function to create a new video object for the socket.emit
+export function setDataNewVideo(currentPlaylist, user, video) {
+  return {
+    room_name: currentPlaylist?.room_name ? currentPlaylist?.room_name : "",
+    user_id: user?.sub,
+    videos: {
+      channels: {
+        channel_id: video?.author?.channelId,
+        channel_pic_url: video?.author?.avatar[0]?.url,
+        channel_title: video?.author?.title,
+      },
+      video_id: video?.videoId,
+      video_length: video?.lengthSeconds,
+      video_pic_url: video?.thumbnails[0]?.url,
+      video_title: video?.title,
+    },
+    _id: currentPlaylist?._id,
+  };
+}
