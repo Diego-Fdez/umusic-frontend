cbf2c6440ae7148f0b2bf857a8b2b0a7965716de
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-17 17:14:05 -0600

    add useEffect

--- src/components/CategoryScreen/components/CategoriesList/CategoriesList.jsx
+++ src/components/CategoryScreen/components/CategoriesList/CategoriesList.jsx
@@ -1,6 +1,6 @@
+import { useState, useEffect } from "react";
 import useSWRImmutable from "swr/immutable";
 import styles from "./styles/CategoriesList.module.css";
-import { Loader } from "@/components";
 import videoStore from "@/store/videoStore";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
@@ -8,27 +8,30 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 const CategoriesList = () => {
   //to add keyword to the videoStore
   const addKeyword = videoStore((state) => state.addKeyword);
+  const [categories, setCategories] = useState([]);
 
   // Fetching categories from the API
-  const { data, isLoading } = useSWRImmutable("/api/v1/categories", fetcher);
+  const { data } = useSWRImmutable("/api/v1/categories", fetcher);
+
+  useEffect(() => {
+    if (data) {
+      setCategories(data?.data);
+    }
+  }, [data]);
 
   return (
     <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <aside className={styles.categoriesContainer}>
-          {data?.data?.map((category) => (
-            <button
-              key={category?.id}
-              className={styles.categoryButton}
-              onClick={() => addKeyword(category?.category_name)}
-            >
-              {category?.category_name?.toUpperCase()}
-            </button>
-          ))}
-        </aside>
-      )}
+      <aside className={styles.categoriesContainer}>
+        {categories?.map((category) => (
+          <button
+            key={category?._id}
+            className={styles.categoryButton}
+            onClick={() => addKeyword(category?.category_name)}
+          >
+            {category?.category_name?.toUpperCase()}
+          </button>
+        ))}
+      </aside>
     </>
   );
 };
