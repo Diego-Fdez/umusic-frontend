fed6502b0ad6c1f0d86513c2cad01487e4ce193d
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-28 20:53:51 -0600

    add export

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as HeadScreen } from './Head/HeadScreen';
 export { default as Loader } from './Loader/Loader';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as VideoCard } from './VideoCard/VideoCard';
+export { default as WithPrivateRoute } from './WithPrivateRoute/WithPrivateRoute';
