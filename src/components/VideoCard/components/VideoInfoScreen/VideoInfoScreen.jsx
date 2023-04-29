76eb148aafe353227ad249746126c9e22b55b237
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-29 11:16:03 -0600

    add loading lazy to image

--- src/components/VideoCard/components/VideoInfoScreen/VideoInfoScreen.jsx
+++ src/components/VideoCard/components/VideoInfoScreen/VideoInfoScreen.jsx
@@ -1,25 +1,25 @@
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import styles from "./styles/VideoInfoScreen.module.css";
-import userStore from "@/store/userStore";
-import tempUserStore from "@/store/tempUserStore";
-import UseVideos from "@/hooks/useVideos";
-import UseWebSocket from "@/hooks/useWebSocket";
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import styles from './styles/VideoInfoScreen.module.css';
+import userStore from '@/store/userStore';
+import tempUserStore from '@/store/tempUserStore';
+import UseVideos from '@/hooks/useVideos';
+import UseWebSocket from '@/hooks/useWebSocket';
 
 const VideoInfoScreen = ({ video }) => {
   const router = useRouter();
   const user = userStore((state) => state.userInfo);
   const tempUserInfo = tempUserStore((state) => state.tempUserInfo);
   const { socket } = UseWebSocket();
-  const { handleAddVideoToList } = UseVideos("", socket);
+  const { handleAddVideoToList } = UseVideos('', socket);
 
   return (
     <div className={styles.videoCardTitle}>
       <Link href={`/watch/${video?.videoId}`}>
         <h4 className={styles.title}>{video?.title.slice(0, 60)}</h4>
       </Link>
-      {router?.pathname === "/channel/[id]" ? (
+      {router?.pathname === '/channel/[id]' ? (
         <></>
       ) : (
         <div className={styles.authorContainer}>
@@ -29,6 +29,7 @@ const VideoInfoScreen = ({ video }) => {
             className={styles.authorImage}
             width={24}
             height={24}
+            loading='lazy'
           />
           <Link href={`/channel/${video?.author?.channelId}`}>
             <h6 className={styles.subtitle}>{video?.author?.title}</h6>
@@ -38,11 +39,11 @@ const VideoInfoScreen = ({ video }) => {
       <div className={styles.statsContainer}>
         <span>
           {video?.stats?.views
-            ? `${Number(video?.stats?.views).toLocaleString("es-US")} views `
-            : "0 views"}
+            ? `${Number(video?.stats?.views).toLocaleString('es-US')} views `
+            : '0 views'}
         </span>
         <span>
-          {video?.publishedTimeText ? ` - ${video?.publishedTimeText}` : ""}
+          {video?.publishedTimeText ? ` - ${video?.publishedTimeText}` : ''}
         </span>
         {user?.sub || tempUserInfo?.sub ? (
           <button
@@ -55,6 +56,7 @@ const VideoInfoScreen = ({ video }) => {
               width={100}
               height={100}
               className={styles.addIcon}
+              loading='lazy'
             />
           </button>
         ) : (
