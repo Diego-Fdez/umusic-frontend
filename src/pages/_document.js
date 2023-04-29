20663599d7fc0ddb2489caae83f934ab64bdb8e9
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-29 11:16:33 -0600

    remove google font script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,16 +5,6 @@ export default function Document() {
     <Html lang='en'>
       <Head>
         <meta httpEquiv='content-language' content='en-US' />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='true'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Fredoka+One&family=Ubuntu:wght@400;700&display=swap'
-          rel='stylesheet'
-        />
       </Head>
       <body>
         <Main />
