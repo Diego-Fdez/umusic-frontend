7c71a5a9b1cc95ddd7e57ed78ab2ee0ad37df330
Diego-Fdez <dieghoxcr7@gmail.com>
2023-04-29 17:00:49 -0600

    use single quotes

--- src/hooks/usePlaylist.js
+++ src/hooks/usePlaylist.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
-import { toast } from "react-toastify";
-import UseFetchFromDB from "./useFetchFromDB";
-import userStore from "@/store/userStore";
-import persistedVideoStore from "@/store/persistedVideoStore";
-import videoStore from "@/store/videoStore";
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
+import UseFetchFromDB from './useFetchFromDB';
+import userStore from '@/store/userStore';
+import persistedVideoStore from '@/store/persistedVideoStore';
+import videoStore from '@/store/videoStore';
 
 const UsePlaylist = () => {
   const router = useRouter();
@@ -19,19 +19,19 @@ const UsePlaylist = () => {
   const rooms = videoStore((state) => state.rooms);
   const videos = videoStore((state) => state.videoList);
   const addVideos = videoStore((state) => state.addVideoList);
-  const [qrImage, setQRImage] = useState("");
-  const [qrDataURL, setQRDataURL] = useState("");
-  const [roomName, setRoomName] = useState("");
+  const [qrImage, setQRImage] = useState('');
+  const [qrDataURL, setQRDataURL] = useState('');
+  const [roomName, setRoomName] = useState('');
 
   //create a new playlist
   const handlerAddPlaylist = async (e) => {
     e.preventDefault();
 
-    const validRoomName = "^[A-Za-z0-9]+$";
+    const validRoomName = '^[A-Za-z0-9]+$';
 
     if (!roomName.match(validRoomName))
       return toast.error(
-        "Invalid playlist name, only numbers and letters are allowed."
+        'Invalid playlist name, only numbers and letters are allowed.'
       );
 
     const setData = {
@@ -39,35 +39,35 @@ const UsePlaylist = () => {
       roomName: roomName,
     };
 
-    const result = await fetchFromDB("/api/v1/room", "POST", setData);
+    const result = await fetchFromDB('/api/v1/room', 'POST', setData);
 
     /* Checking if there is an error in the response from the server. If there is an error, it will
   display the error message and clear the input field. */
     if (result?.data?.error) {
-      setRoomName("");
+      setRoomName('');
       return toast.error(result?.data?.error);
     }
 
     //if there is an error, display the error
     if (error) {
-      setRoomName("");
+      setRoomName('');
       return toast.error(error);
     }
 
     //if there is no error, display the success message
-    toast.success("Your room has been created successfully!");
-    setRoomName("");
+    toast.success('Your room has been created successfully!');
+    setRoomName('');
   };
 
   //if the user has no playlists, it will create one.
   const handlerSaveDefaultPlaylist = async (roomId) => {
     const result = await fetchFromDB(
       `/api/v1/user-configs/${user?.sub}`,
-      "GET"
+      'GET'
     );
 
-    if (result?.data?.error === "No configs found") {
-      const newUserConfigs = await fetchFromDB("/api/v1/user-configs", "POST", {
+    if (result?.data?.error === 'No configs found') {
+      const newUserConfigs = await fetchFromDB('/api/v1/user-configs', 'POST', {
         userId: user?.sub,
         roomId: roomId,
       });
@@ -85,9 +85,9 @@ const UsePlaylist = () => {
   //It fetches the playlists from the database.
   const handlerGetAllPlaylists = async () => {
     /* Checking if the user is logged in. If not, it will display a toast message. */
-    if (!user?.sub) return toast.info("Please login to view your playlists");
+    if (!user?.sub) return toast.info('Please login to view your playlists');
 
-    const result = await fetchFromDB("/api/v1/my-rooms", "POST", {
+    const result = await fetchFromDB('/api/v1/my-rooms', 'POST', {
       userId: user?.sub,
     });
 
@@ -102,7 +102,7 @@ const UsePlaylist = () => {
 
   //function to delete a playlist.
   const deleteOnePlaylist = async (roomId) => {
-    const result = await fetchFromDB(`/api/v1/room/${roomId}`, "DELETE");
+    const result = await fetchFromDB(`/api/v1/room/${roomId}`, 'DELETE');
 
     //if the result is an error, display it.
     if (result?.data?.error) return toast.error(result?.data?.error);
@@ -111,16 +111,19 @@ const UsePlaylist = () => {
     //filter the rooms array to remove the deleted playlist.
     const filteredRooms = rooms.filter((room) => room._id !== roomId);
     setRooms(filteredRooms);
-    toast.success("Playlist deleted successfully");
+    toast.success('Playlist deleted successfully');
   };
 
   //function to delete a video from the playlist
-  const handlerDeleteVideo = async (videoId) => {
-    const result = await fetchFromDB("/api/v1/room", "DELETE", { id: videoId });
+  const handlerDeleteVideo = async (videoId, roomId) => {
+    const result = await fetchFromDB(
+      `/api/v1/my-rooms/${roomId}/${videoId}`,
+      'DELETE'
+    );
 
     //is checking if there is an error returned from the server
     if (error) return toast.error(error);
-    if (result?.status === "FAILED") return toast.error(result?.data?.error);
+    if (result?.status === 'FAILED') return toast.error(result?.data?.error);
 
     //filter out the video that is deleted from the list of videos in the store.
     const filteredVideos = videos.filter(
@@ -131,16 +134,16 @@ const UsePlaylist = () => {
     toast.success(result?.data);
 
     //is checking if the playlist is empty.
-    if (filteredVideos.length === 0) router.push("/room");
+    if (filteredVideos.length === 0) router.push('/room');
   };
 
   //function to delete all videos from the playlist
   const handlerDeleteAllVideos = async (id) => {
-    const result = await fetchFromDB(`/api/v1/room/${id}`, "PUT");
+    const result = await fetchFromDB(`/api/v1/room/${id}`, 'PUT');
 
     //is checking if there is an error returned from the server
     if (error) return toast.error(error);
-    if (result?.status === "FAILED") return toast.error(result?.data?.error);
+    if (result?.status === 'FAILED') return toast.error(result?.data?.error);
 
     //set the store to an empty array to clear the playlist.
     addVideos([]);
@@ -148,7 +151,7 @@ const UsePlaylist = () => {
     toast.success(result?.data);
 
     //redirect to the room page.
-    router.push("/room");
+    router.push('/room');
   };
 
   //function to get the QR code.
@@ -161,15 +164,15 @@ const UsePlaylist = () => {
     };
 
     if (
-      (user?.sub !== "") &
-      (token !== "") &
-      (currentPlaylist?._id !== "") &
-      (qrImage === "")
+      (user?.sub !== '') &
+      (token !== '') &
+      (currentPlaylist?._id !== '') &
+      (qrImage === '')
     ) {
-      const result = await fetchFromDB(`/api/v1/qr`, "POST", setData);
+      const result = await fetchFromDB(`/api/v1/qr`, 'POST', setData);
 
       /* This is checking if there is an error. If there is, it will return an error message. */
-      if (result?.status === "FAILED") return toast.error(result?.data?.error);
+      if (result?.status === 'FAILED') return toast.error(result?.data?.error);
       if (error) return toast.error(error);
 
       setQRDataURL(result?.data?.linkURL);
